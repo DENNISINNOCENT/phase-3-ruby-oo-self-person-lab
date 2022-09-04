08527acae99e12a97ec2e6a672ed11664be75534
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-09-04 20:13:32 +0300

    commit tree 55d5f17dbaf5cfc46770544a73549aedf1d09d63

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :happiness
+    attr_accessor :hygiene
+    attr_accessor :bank_account
+    def initialize(name,happiness=8,bank_account=$25,hygiene=8)
+      @name = name
+  end
+
+  
+end
